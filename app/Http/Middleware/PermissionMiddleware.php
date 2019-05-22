228dfebc18e05e8f26c5eda5ac9053b2ceb47bea
team-sattit <otoporamibd@gmail.com>
2019-05-22 11:16:56 +0600

    Permission Middleware

--- app/Http/Middleware/PermissionMiddleware.php
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -14,7 +14,7 @@ class PermissionMiddleware {
 	 */
 	public function handle($request, Closure $next) {
 
-		/*$routeName = $request->route()->getName();
+		$routeName = $request->route()->getName();
 
 			if (!$request->user()->can($routeName)) {
 				if ($request->ajax()) {
@@ -30,25 +30,7 @@ class PermissionMiddleware {
 
 				Auth::logout();
 				return redirect()->route('login');
-		*/
-
-		// $routeName = $request->route()->getName();
-
-		// if (!$request->user()->can($routeName)) {
-		// 	if ($request->ajax()) {
-		// 		return response('Access denied!', 401);
-		// 	}
-		// 	abort(401);
-		// }
-
-		// //check for user force logout
-		// if ($request->user()->force_logout) {
-		// 	$request->user()->force_logout = 0;
-		// 	$request->user()->save();
-
-		// 	Auth::logout();
-		// 	return redirect()->route('login');
-		// }
+		
 
 		return $next($request);
 	}
